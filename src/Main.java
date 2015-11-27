914606ea3bf8e29649b49df4f32cab4d543dc095
Rikysamuel <rikz.samuel@gmail.com>
2015-11-27 22:28:18 +0700

    multi layer perceptron done

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import Util.Util;
+import Util.Options;
 import weka.core.Instances;
 
 /**
@@ -7,12 +8,26 @@ import weka.core.Instances;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Util.loadARFF("data/weather.numeric.arff");
-//        Util.loadARFF("data/train.arff");
+        Options.biasValue = 1;
+        Options.biasWeight = 0.1;
+        Options.initWeight = 0.1;
+        Options.numOfHiddenNeuron = 2;
+        Options.momentum = 0.1;
+        Options.learningRate = 0.1;
+        Options.maxEpoch = 1000;
+//        Options.MSEthreshold = 0.3387570349024238;
+
+//        Util.loadARFF("data/simple.weather.arff");
+//        Util.loadARFF("data/weather.nominal.arff");
+        Util.loadARFF("data/iris.arff");
+//        Util.loadARFF("data/weather.numeric.arff");
+
+        Instances data = Util.setNominalToBinary(Util.getData());
+        data = Util.useNormalization(data);
+        Util.setData(data);
+
         Util.buildModel("mlp");
 
-//        Util.loadARFF("data/test.weather.numeric.arff");
-//        Instances test = Util.setNominalToBinary(Util.getData());
-        Util.FullSchema(Util.getData());
+        Util.FullSchema(data);
     }
 }
---
+++ src/Util/Options.java
@@ -0,0 +1,24 @@
+package Util;
+
+/**
+ * Helper class to define the topology of the ANN
+ * Created by rikysamuel on 11/27/2015.
+ */
+public class Options {
+    /* the bias value */
+    public static double biasValue;
+    /* the bias weight */
+    public static double biasWeight;
+    /* the weight initial value */
+    public static double initWeight;
+    /* num of hidden neuron, if algorithm used is multi layer perceptron*/
+    public static int numOfHiddenNeuron;
+    /* momentum value */
+    public static double momentum;
+    /* learning rate value */
+    public static double learningRate;
+    /* num of max epoch */
+    public static int maxEpoch;
+    /* the MSE threshold */
+    public static double MSEthreshold;
+}
